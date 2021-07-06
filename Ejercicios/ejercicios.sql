250bea356559e61917c6af550621c703da35cf58
Juan Herrera <juannicolash01@gmail.com>
2021-07-06 13:27:40 -0300

    Base de datos y ej 1

---
+++ Ejercicios/ejercicios.sql
@@ -0,0 +1,11 @@
+# Comentario de una linea
+/*
+    Comentario multilinea
+*/
+
+/*
+    Seleccionar todas las tuplas de actor, mostrar su columna de first_name como Nombre y last_name como Apellido
+*/
+
+SELECT actor_id, first_name AS Nombre, last_name AS Apellido, last_update FROM sakila.actor;
+
